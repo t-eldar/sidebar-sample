0707bb7c17bc61d62288a756935dd2e5a72b1230
t-eldar <teldar6077@gmail.com>
2023-12-25 13:27:40 +0300

    Fixed bug

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
+const navLinks = document.querySelector(".nav-links");
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 768) {
+    navLinks.style.display = "flex";
+    return;
+  }
+
+  navLinks.style.display = "none";
+});
+
 const toggleMenu = () => {
   const element = document.querySelector(".nav-links");
   const icon = document.querySelector(".gg-menu-motion");
 
-  console.log(element.style.visibility);
-  if (
-    element.style.visibility === "hidden" ||
-    element.style.visibility === ""
-  ) {
-    element.style.visibility = "visible";
+  if (!element.checkVisibility()) {
+    element.style.display = "flex";
     icon.animate([{ transform: "rotate(0)" }, { transform: "rotate(90deg)" }], {
       duration: 300,
       fill: "forwards",
     });
   } else {
-    element.style.visibility = "hidden";
+    element.style.display = "none";
 
     icon.animate([{ transform: "rotate(90deg)" }, { transform: "rotate(0)" }], {
       duration: 300,
